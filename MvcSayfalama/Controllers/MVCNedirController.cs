598c306eedabaa1693e50ffc80ca8ba4cc777f2b
ecesanal <elifecesanal4@gmail.com>
2024-10-03 19:25:18 +0300

    first commit

---
+++ MvcSayfalama/Controllers/MVCNedirController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSayfalama.Controllers
+{
+    public class MVCNedirController : Controller
+    {
+        // GET: MVCNedir
+        public ActionResult Nedir()
+        {
+            return View();
+        }
+        public ActionResult Faydalari()
+        {
+            return View();
+        }
+        public ActionResult OrnekSiteler()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcSayfalama/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcSayfalama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvcSayfalama")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3328aca9-57c3-46a2-8ed8-cb374477154f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
