a5760d600d2f5261f8c2e35d4339452de0d3f3b0
ecesanal <elifecesanal4@gmail.com>
2024-10-10 13:17:03 +0300

    new updates paging

---
+++ MvcSayfalama/Controllers/MatematikController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSayfalama.Controllers
+{
+    public class MatematikController : Controller
+    {
+        // GET: Matematik
+        public string Topla(int? sayi1 , int? sayi2)
+        {
+            if (sayi1.HasValue && sayi2 != null)
+                return (sayi1.Value + sayi2.Value).ToString();
+            else
+                return "İki sayı giriniz!";
+        }
+        public int TEksekareCiftseKup(int sayi)
+        {
+            if (sayi % 2 == 0)
+                return sayi * sayi * sayi;
+            else
+                return sayi * sayi;
+        }
+    }
+}
